ef997a3cd79ffed4267f1ad71ad87a604c071179
Akash Chauhan <akashchauhan087@gmail.com>
2025-02-20 20:59:13 +0530

    library project removed

--- django/ItemShirt/ItemShirtApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Item, Size
-
-# Register your models here.
-admin.site.register(Item)
-admin.site.register(Size)
--- django/ItemShirt/ItemShirtApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ItemshirtappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ItemShirtApp'
